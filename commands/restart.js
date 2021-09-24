6e6c56a22708b26903948298345b0dea2daebddd
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-24 22:01:09 +0530

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "restart",
+  description: "Restart the Bot ( use this command is there is any issue with the bot) ",
+  usage: "",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: [],
+  },
+  aliases: ["rs", "rst"],
+  run: async (client, message, args) => {
+		if (message.author.id !== '746777830121013349') {
+			return message.channel.send('You cannot use this command!');
+		}
+		await message.channel.send('Restarting bot...');
+		return process.exit();
+	},
