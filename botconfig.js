8ff579e1e191ad544497b3cd84c7d05ea01981f1
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-28 03:02:51 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -80,7 +80,7 @@ module.exports = {
   },*/
      Lavalink: {
     id: "Main",
-    host: "152.70.180.50",
+    host: "10.0.0.151",
     port: 2333,
     pass: "helloworld", 
     secure: false, // Set this to true if you're self-hosting lavalink on replit.
