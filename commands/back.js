b0def45738581ed44caf9e1fff5a1761f5885142
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-30 11:38:31 +0530

    Create back.js

---
+++ commands/back.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const { TrackUtils } = require("erela.js");
+
+module.exports = {
+    name: 'back',
+    aliases: ['previous'],
+    description: "Goes back to the previous song.",
+    utilisation: '{prefix}back',
+    voiceChannel: true,
+
+    async execute(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        if (!queue.previousTracks[1]) return message.channel.send(`There was no music played before ${message.author}... try again ? ❌`);
+
+        await queue.back();
+
+        message.channel.send(`Playing the **previous** track ✅`);
+    },
+};
