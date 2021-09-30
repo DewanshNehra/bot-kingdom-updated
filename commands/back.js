7f8d1bda4d03495936816af44a71a97f1fac7ff7
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-30 14:36:21 +0530

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -15,11 +15,11 @@ module.exports = {
         let player = await client.Manager.get(message.guild.id);
        
         
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!player.queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
-        if (!queue.previousTracks[1]) return message.channel.send(`There was no music played before ${message.author}... try again ? ❌`);
+        if (!player.queue.previousTracks[1]) return message.channel.send(`There was no music played before ${message.author}... try again ? ❌`);
 
-        await queue.back();
+        await player.queue.back();
 
         message.channel.send(`Playing the **previous** track ✅`);
     },
