ce59201a82b91a0b7f85117448107290ab1c23da
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-30 15:33:44 +0530

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -14,8 +14,9 @@ module.exports = {
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
         if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
-        const queue = player.getQueue(message.guild.id);
         console.log(player)
+        const queue = player.getQueue(message.guild.id);
+        console.log(error)
         
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
