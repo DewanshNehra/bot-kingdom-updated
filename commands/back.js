6eaa92bde0adb273399b1948c37b94e26a0d8ad0
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-30 11:57:28 +0530

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -12,12 +12,18 @@ module.exports = {
         channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
         member: [],
     },
+    /**
+     *
+     * @param {import("../structures/DiscordMusicBot")} client
+     * @param {import("discord.js").Message} message
+     * @param {string[]} args
+     * @param {*} param3
+     */
 
     run: async (client, message, args, { GuildDB }) => {
-        const queue = player.getQueue(message.guild.id);
         let player = await client.Manager.get(message.guild.id);
-
-
+        const queue = player.getQueue(message.guild.id);
+        
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         if (!queue.previousTracks[1]) return message.channel.send(`There was no music played before ${message.author}... try again ? ❌`);
