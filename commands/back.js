c38564ab42af3dd835aea33ec92d61ca27936386
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-09-30 12:23:30 +0530

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -19,21 +19,21 @@ module.exports = {
      * @param {*} param3
      */
     run: async (client, message, args, { GuildDB }) => {
-    const player = bot.manager.get(message.guild.id);
+    const player = client.manager.get(message.guild.id);
 
     if (!player)
-      return bot.say.ErrorMessage(message, "The bot is currently not playing.");
+      return client.say.ErrorMessage(message, "The bot is currently not playing.");
 
-    if (!bot.canModifyQueue(message)) return;
+    if (!client.canModifyQueue(message)) return;
 
     const track = player.queue.previous;
 
     if (!track)
-      return bot.say.WarnMessage(message, "No previous track was found.");
+      return client.say.WarnMessage(message, "No previous track was found.");
 
     player.queue.add(track, 0);
     player.stop();
 
-    return bot.say.QueueMessage(bot, player, "Backed to the previous song.");
+    return client.say.QueueMessage(client, player, "Backed to the previous song.");
   }
 };
