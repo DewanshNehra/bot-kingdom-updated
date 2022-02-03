72f44860ee628b2c1632981a24345c4ab06da815
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2022-02-03 16:21:14 +0530

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -20,7 +20,7 @@ module.exports = {
         if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to play something!**");
         if(!message.member.voice.channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(message.channel, "❌ | **Bot doesn't have Create Invite Permission**");
 
-        let Invite = await message.member.voice.channel.activityInvite("755600276941176913")//Made using discordjs-activity package
+        let Invite = await message.member.voice.channel.activityInvite("880218394199220334")//Made using discordjs-activity package
         let embed = new MessageEmbed()
         .setAuthor("YouTube Together", "https://cdn.discordapp.com/emojis/749289646097432667.png?v=1")
         .setColor("#FF0000")
@@ -50,7 +50,7 @@ __**[Join YouTube Together](https://discord.com/invite/${Invite.code})**__
             if (!member.voice.channel) return client.sendTime(interaction, "❌ | You must be in a voice channel to use this command.");
             if(!member.voice.channel.permissionsFor(guild.me).has("CREATE_INSTANT_INVITE"))return client.sendTime(interaction, "❌ | **Bot doesn't have Create Invite Permission**");
 
-            let Invite = await member.voice.channel.activityInvite("755600276941176913")//Made using discordjs-activity package
+            let Invite = await member.voice.channel.activityInvite("880218394199220334")//Made using discordjs-activity package
             let embed = new MessageEmbed()
             .setAuthor("YouTube Together", "https://cdn.discordapp.com/emojis/749289646097432667.png?v=1")
             .setColor("#FF0000")
